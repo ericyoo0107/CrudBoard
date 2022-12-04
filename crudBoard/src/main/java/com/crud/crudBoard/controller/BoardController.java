14cbb02d5766a179f670c887d92a318db2dbe122
YuJunHyuk <ericyoo0107@gmail.com>
2022-12-04 13:09:31 +0900

    <feat> 게시글 수정 기능

--- crudBoard/src/main/java/com/crud/crudBoard/controller/BoardController.java
+++ crudBoard/src/main/java/com/crud/crudBoard/controller/BoardController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.crud.crudBoard.entity.Board;
@@ -30,16 +31,33 @@ public class BoardController {
 		model.addAttribute("list", boardService.getList());
 		return "boardList";
 	}
-
+	//HTML : boardView
 	@GetMapping("/board/view") ///board/view?id=3 하면 id에 3들어감
 	public String boardView(Model model, Integer id) {
 		model.addAttribute("article", boardService.getBoard(id));
 		return "boardView";
 	}
 
-	@GetMapping("/board/delete") ///board/delete?id=3 하면 id에 3들어감
+	@GetMapping("/board/delete") // /board/delete?id=3 하면 id에 3들어감
 	public String boardDelete(Integer id) {
 		boardService.delete(id);
+		return "redirect:/board/list";
+	}
+
+	//HTML : boardModify
+	@GetMapping("/board/modify/{id}")
+	public String boardModify(@PathVariable("id") Integer id, Model model) {
+		model.addAttribute("board", boardService.getBoard(id));
+		return "boardModify";
+	}
+
+	@PostMapping("/board/update/{id}")
+	public String boardUpdate(@PathVariable("id") Integer id, Board board)
+	{
+		Board tempBoard = boardService.getBoard(id);
+	 	tempBoard.setTitle(board.getTitle());
+		tempBoard.setContent(board.getContent());
+		boardService.write(tempBoard);
 		return "redirect:/board/list";
 	}
 }
